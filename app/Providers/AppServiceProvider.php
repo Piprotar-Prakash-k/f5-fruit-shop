2f381637db10fc38ee4d84007d31c56ff6d68a4b
Piprotar-Prakash-k <piprotarprakash9@gmail.com>
2026-06-04 17:37:32 +0530

    fix livewire upload middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,24 +4,26 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        Livewire::setUpdateRoute(function ($handle) {
+            return \Illuminate\Support\Facades\Route::post(
+                '/livewire/update',
+                $handle
+            )->middleware('web');
+        });
     }
 }
